67896bea4642cf8bd4f2d77238111094bb4d2bc0
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2019-12-07 16:08:47 -0500

    Added the line of best fit stuff

--- Application/main.py
+++ Application/main.py
@@ -23,3 +23,39 @@ try:
 except KeyboardInterrupt:
     ser.close()
 
+
+finalMinRValue = 0.0;
+finalMinMValue = 0.0;
+
+# linear regression is just minimizing the distance from point to line
+
+def findRValue(mValue, xOneValue, yOneValue, dataPointsY, i):
+
+    if(i >= len(dataPointsY)):
+        return 0;
+
+    if(mValue != 0):
+        x = (i/mValue + dataPointsY[i] + mValue*xOneValue - yOneValue)/(mValue+(1/mValue));
+        y = mValue*(x-xOneValue)+yOneValue
+
+        return (dataPointsY[i]-y)**2 + (i-x)**2 + findRValue(mValue, xOneValue, yOneValue, dataPointsY, i+1);
+
+    return dataPointsY[i] + findRValue(mValue, xOneValue, yOneValue, dataPointsY, i+1);
+
+def findBestLine(allDataPointsY):
+    mValue = 0.0
+    minRValue = 1000.0;
+    for i in range(0, len(allDataPointsY)):
+        for j in range(i+1, len(allDataPointsY)):
+            mEval = (allDataPointsY[j] - allDataPointsY[i])/(j-i);
+            rEval = findRValue(mEval, j, allDataPointsY[j], allDataPointsY, 0);
+            if(rEval < minRValue):
+                minRValue = rEval
+                mValue = mEval
+
+    global finalMinRValue
+    finalMinRValue = minRValue;
+    global finalMinMValue
+    finalMinMValue = mValue;
+
+    return mValue < 0 and abs(mValue) > 1;
